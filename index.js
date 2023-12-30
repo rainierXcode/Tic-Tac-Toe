d5ba40be9328367d52ec1751add119028d80a606
Rainier Sapin <rainiersapin0131@gmail.com>
2023-12-30 23:10:31 +0800

    Update UI

--- index.js
+++ index.js
@@ -1,145 +1,190 @@
-const game = ( function (){
-
-  let gameboardArray = ["", "", "", "", "", "", "", "", ""]
-
-  let winningPossibilities =
-  [
-      //row
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-
-      //col
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-
-      //slant
-      [0,4,8],
-      [2,4,6]
-  ]
-
-  function updateBox(currentPlayer) {
-      const thisBox = this.querySelector("img");
-      thisBox.src = "../markers/mark" + currentPlayer.marker + ".svg";
-      const gridNum = parseInt(thisBox.getAttribute("data-num"))
-      this.classList.add("unclickable")
-      gameboardArray[ gridNum -1 ] = currentPlayer.marker;
-  }
-  
-  function reset(){
-      const boxGrid = document.querySelectorAll(".gameboard-grid div");
-      boxGrid.forEach(box =>{
-          box.classList.remove("unclickable")
-          boxImg = box.querySelector("img")
-          boxImg.src = ""
-      })
-      gameboardArray = gameboardArray.map(() => "");
-
-  }
-
-  function checkWinner(){
-      for(let i = 0; i < winningPossibilities.length; i++){
-          option = winningPossibilities[i];
-          firstGrid = option[0];
-          secondGrid = option[1];
-          thirdGrid = option[2]
-          if(gameboardArray[firstGrid] == "" || gameboardArray[secondGrid] == "" || gameboardArray[thirdGrid] == ""){
-              continue;
-          }
-          
-          if( gameboardArray[firstGrid] === gameboardArray[secondGrid] && gameboardArray[secondGrid] === gameboardArray[thirdGrid]){
-              console.log("win")
-              return gameboardArray[firstGrid]
-          }
-      }
-      
-      if(!gameboardArray.includes("")){
-          return "Draw"
-      }
-
-      return "Playing"
-  }
-
-  function changePlayer(currentPlayer,p1,p2){
-      return (currentPlayer == p1) ? p2 : p1;
-  }
-
-  function statusUpdate(status){
-      const statusName = document.getElementById("status")
-      statusName.textContent =  status
-  }
-
-  function handleGameResult(result,p1,p2){
-      switch(result){
-          case p1.marker:
-              statusUpdate(p1.name + " Win!")
-              break;
-          case p2.marker:
-              statusUpdate(p2.name + " Win!")
-              break;
-          case "Draw":
-              statusUpdate("Draw")
-              break;
-          default:
-              statusUpdate(currentPlayer.name + " Turn")
-              return; 
-      }
-      reset()
-  }
-  
-  function playGame(p1,p2) {
-      const boxGrid = document.querySelectorAll(".gameboard-grid div");
-      currentPlayer = p1;
-      statusUpdate(currentPlayer.name + " Turn")
-
-      boxGrid.forEach(box =>{
-          box.addEventListener('click', function() {
-              updateBox.call(this, currentPlayer);
-              result = checkWinner()
-              currentPlayer = changePlayer(currentPlayer,p1,p2);
-              handleGameResult(result,p1,p2)
-      
-              
-          });
-          
-      })
-  }
-
-  return  {
-     playGame: (p1, p2) => playGame(p1, p2)
-  };
-  
-  })();
-  
-  
-  
-function Player(name, marker){
-  this.name = name;
-  this.marker = marker;
+function hideBgMarkers() {
+    const bgMarkers = document.querySelectorAll(".bgMark");
+    bgMarkers.forEach(mark => {
+        mark.style.display = "none"
+    })
 }
-  
 
-function openGame(){
-  const player1 = new Player("Player 1", "X")
-  const player2 = new Player("Player 2", "O")
-      
-  const startingTitle = document.querySelector(".startingTitle")
-  const gameBox = document.querySelector(".game-box")
 
-  startingTitle.style.display = "none"
-  playButton.style.display = "none"
-  gameBox.style.display = "flex"
-  game.playGame(player1, player2)
+
+const game = (function () {
+
+    let gameboardArray = ["", "", "", "", "", "", "", "", ""]
+
+    let winningPossibilities =
+        [
+            //row
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+
+            //col
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+
+            //slant
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+    function updateBox(currentPlayer) {
+        const thisBox = this.querySelector("img");
+        thisBox.style.opacity = 1;
+        thisBox.src = "../markers/mark" + currentPlayer.marker + ".svg";
+        const gridNum = parseInt(thisBox.getAttribute("data-num"))
+        this.classList.add("unclickable")
+        gameboardArray[gridNum - 1] = currentPlayer.marker;
+    }
+
+    function reset() {
+        const boxGrid = document.querySelectorAll(".gameboard-grid div");
+        boxGrid.forEach(box => {
+            box.classList.remove("unclickable")
+            boxImg = box.querySelector("img")
+            boxImg.src = ""
+        })
+        gameboardArray = gameboardArray.map(() => "");
+
+    }
+
+    function checkWinner() {
+        for (let i = 0; i < winningPossibilities.length; i++) {
+            option = winningPossibilities[i];
+            firstGrid = option[0];
+            secondGrid = option[1];
+            thirdGrid = option[2]
+            if (gameboardArray[firstGrid] == "" || gameboardArray[secondGrid] == "" || gameboardArray[thirdGrid] == "") {
+                continue;
+            }
+
+            if (gameboardArray[firstGrid] === gameboardArray[secondGrid] && gameboardArray[secondGrid] === gameboardArray[thirdGrid]) {
+                console.log("win")
+                return gameboardArray[firstGrid]
+            }
+        }
+
+        if (!gameboardArray.includes("")) {
+            return "Draw"
+        }
+
+        return "Playing"
+    }
+
+    function changePlayer(currentPlayer, p1, p2) {
+        return (currentPlayer == p1) ? p2 : p1;
+    }
+
+    function statusUpdate(status) {
+        const p1Turn = document.getElementById("p1-turn");
+        const p2Turn = document.getElementById("p2-turn");
+
+        switch (status) {
+            case "X":
+                p1Turn.textContent = "Your Turn";
+                p2Turn.textContent = "";
+                return;
+            case "O":
+                p2Turn.textContent = "Your Turn";
+                p1Turn.textContent = "";
+                return;
+        }
+    }
+
+    function handleGameResult(result, p1, p2) {
+        const statusName = document.getElementById("status")
+        switch (result) {
+            case p1.marker:
+                statusName.textContent = p1.name + " Win!";
+                break;
+            case p2.marker:
+                statusName.textContent = p2.name + " Win!";
+                statusUpdate(p2.name + " Win!")
+                break;
+            case "Draw":
+                statusUpdate("Draw")
+                break;
+            default:
+                statusUpdate(currentPlayer.marker)
+                return;
+        }
+        reset()
+    }
+
+    function playGame(p1, p2) {
+
+        const boxGrid = document.querySelectorAll(".gameboard-grid div");
+        currentPlayer = p1;
+        statusUpdate(currentPlayer.name + " Turn")
+
+        boxGrid.forEach(box => {
+            box.addEventListener('click', function () {
+                updateBox.call(this, currentPlayer);
+                result = checkWinner()
+                currentPlayer = changePlayer(currentPlayer, p1, p2);
+                handleGameResult(result, p1, p2)
+
+
+            });
+
+            box.addEventListener("mouseenter", () => {
+                const boxImg = box.querySelector("img");
+                boxImg.src = "../markers/mark" + currentPlayer.marker + ".svg";
+                boxImg.style.opacity = 0.35;
+            })
+
+            box.addEventListener("mouseleave", () => {
+                const computedStyle = window.getComputedStyle(box);
+
+                const pointerEventsValue = computedStyle.getPropertyValue('pointer-events');
+
+                if (pointerEventsValue !== 'none') {
+                     const boxImg = box.querySelector("img");
+                     boxImg.src = "";
+                }
+            })
+
+
+        })
+
+
+
+
+
+    }
+
+    return {
+        playGame: (p1, p2) => playGame(p1, p2)
+    };
+
+})();
+
+
+
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
 }
+
+
+function openGame() {
+    const player1 = new Player("Player 1", "X")
+    const player2 = new Player("Player 2", "O")
+
+    hideBgMarkers();
+
+    const startingTitle = document.querySelector(".startingTitle")
+    const gameBox = document.querySelector(".game-box")
+
+    startingTitle.style.display = "none"
+    playButton.style.display = "none"
+    gameBox.style.display = "flex"
+    game.playGame(player1, player2)
+}
+
 
 
 const playButton = document.querySelector(".play-button-container")
 playButton.addEventListener("click", openGame);
 
 
-
-
-  
-  
-  
